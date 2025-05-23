8368e90ef45ff6f13d7846b126a28921f4651d1a
Om Mehta <ommehta16@icloud.com>
2025-05-22 23:02:46 -0400

    Start adding stuff to website (math team rn... will switch to tourney on next commit lol)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ document.querySelectorAll("h2").forEach(el => {
     el.addEventListener("click", () => {
         el.scrollIntoView({
             behavior: "smooth",
-            block: "start",
+            block: "center",
         });
         setTimeout(() => { window.location.hash = el.id; }, 10);
     });
@@ -34,4 +34,49 @@ const updateNav = () => {
     }
 }
 
+const goToSection = () => {
+    if (!window.location.hash) return;
+    el = document.querySelector(window.location.hash);
+    if (!el) return;
+    el.addEventListener("click", () => {
+        el.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+        });
+        setTimeout(() => { window.location.hash = el.id; }, 10);
+    });
+}
+
 document.addEventListener("scroll", updateNav);
+
+let menuShown = false;
+const menu = document.querySelector("#menu");
+
+const hideMenu = () => {
+    document.querySelector("nav").classList.remove("big");
+    document.body.classList.remove("noscroll");
+    menu.classList.add("fa-bars");
+    menu.classList.remove("fa-xmark");
+}
+
+const showMenu = () => {
+    document.querySelector("nav").classList.add("big");
+    document.body.classList.add("noscroll");
+    menu.classList.add("fa-xmark");
+    menu.classList.remove("fa-bars");
+}
+
+menu.addEventListener("click", () => {
+    if (menuShown) hideMenu();
+    else showMenu();
+    menuShown = !menuShown;
+});
+
+setTimeout(updateNav, 1000);
+
+window.addEventListener("hashchange", e => {
+    e.preventDefault();
+    goToSection();
+});
+
+goToSection();
